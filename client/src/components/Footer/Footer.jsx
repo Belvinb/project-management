265ee8a730c81b0d011569fa97c5886b39dc5b29
belvinb <belvinbbenny@gmail.com>
2022-09-10 11:35:32 +0530

    added footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+
+function Copyright() {
+    return (
+      <Typography variant="body2" color="text.secondary">
+        {"Copyright © "}
+        <link>
+        </link>
+      </Typography>
+    );
+}
+
+
+
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
