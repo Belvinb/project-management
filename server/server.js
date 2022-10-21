ddaffe2b10995168995ffcccf568949896b1ed09
belvinb <belvinbbenny@gmail.com>
2022-10-21 17:06:56 +0530

    bug fixed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require("path");
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const cors = require("cors");
