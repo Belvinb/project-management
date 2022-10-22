eae743f7ca413942606b043b9b05e9d6708a1589
belvinb <belvinbbenny@gmail.com>
2022-10-22 13:28:41 +0530

    bug fixed

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,10 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app){
-    app.use(proxy("/socket"), {
+    app.use(proxy("/socket.io"), {
       target: "http://localhost:8800",
       changeOrigin: true,
-      ws: true,
-      logLevel: "debug",
+      ws: false,
     });
 }
 
--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,7 +2,11 @@ import io from "socket.io-client"
 
 // export const socket = io("http://localhost:8800");
 
-export const socket = io('/',{
-    transports:['websocket'],
-    path:"/socket"
+// export const socket = io('/',{
+//     transports:['websocket'],
+//     path:"/socket"
+// })
+
+export const socket = io({
+    protocols : ["http"]
 })
